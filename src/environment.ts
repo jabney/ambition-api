d9d03b1c569e0191e35b26dd13c9c5135e2c09bc
James Abney <james.abney@gmail.com>
2019-12-07 11:28:24 -0800

    Add api key variable

--- src/environment.ts
+++ src/environment.ts
@@ -2,6 +2,7 @@ import { booleanize, integerize } from './lib/env-helpers'
 
 const ALLOW_UNSECURE = booleanize(process.env.ALLOW_UNSECURE)
 const TRUST_PROXY = integerize(process.env.TRUST_PROXY, 0)
+const API_KEY = process.env.API_KEY
 
 export default Object.freeze({
   /**
@@ -12,4 +13,8 @@ export default Object.freeze({
    * Determines trust proxy hops for app configuration.
    */
   TRUST_PROXY,
+  /**
+   * Api key for protecting some auth routes.
+   */
+  API_KEY,
 })
