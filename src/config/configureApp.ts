3e489e247241c5977754f4b6f62615775d587aa2
James Abney <james.abney@gmail.com>
2019-12-07 08:57:14 -0800

    Set app trust proxy

--- src/config/configureApp.ts
+++ src/config/configureApp.ts
@@ -1,12 +1,19 @@
 import express, { Application } from 'express'
 import logger from 'morgan'
 import rejectHttp from '../middleware/reject-http'
+import env from '../environment';
 
 /**
  * Set up application middleware and misc.
  */
 function configureApp(app: Application) {
   app.set('x-powered-by', null)
+
+  // Set trust proxy for heroku, etc.
+  if (env.TRUST_PROXY > 0) {
+    app.set('trust proxy', env.TRUST_PROXY)
+  }
+
   app.use(logger('dev'));
   app.use(express.json({ limit: '1kb'}));
 
