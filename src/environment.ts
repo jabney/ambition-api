da6e6103e32b3a02b4e05d85ae4a677f94dd2a28
James Abney <james.abney@gmail.com>
2019-12-07 08:57:22 -0800

    Add env variable

--- src/environment.ts
+++ src/environment.ts
@@ -1,10 +1,15 @@
-import { booleanize } from './lib/env-helpers'
+import { booleanize, integerize } from './lib/env-helpers'
 
-const ALLOW_UNSECURE = booleanize(process.env.ALLOW_HTTP)
+const ALLOW_UNSECURE = booleanize(process.env.ALLOW_UNSECURE)
+const TRUST_PROXY = integerize(process.env.TRUST_PROXY, 0)
 
 export default Object.freeze({
   /**
    * Don't reject non-https requests.
    */
   ALLOW_UNSECURE,
+  /**
+   * Determines trust proxy hops for app configuration.
+   */
+  TRUST_PROXY,
 })
