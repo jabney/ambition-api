cee7e343fe67e5a48f2dd06c0e11a33878a05580
James Abney <james.abney@gmail.com>
2019-12-07 09:02:17 -0800

    Refactor type annotations

--- src/middleware/reject-http.ts
+++ src/middleware/reject-http.ts
@@ -1,12 +1,12 @@
-import exp from 'express'
+import { RequestHandler } from 'express'
 import { createError } from '../lib/errors'
 import env from '../environment'
 
 /**
  * Reject http requests on any server which sets x-forwarded-proto to 'https'.
  */
-export default function rejectHttp() {
-  return function (req: exp.Request, res: exp.Response, next: exp.NextFunction) {
+export default function rejectHttp(): RequestHandler {
+  return function (req, res, next) {
     if (!req.secure && !env.ALLOW_UNSECURE) {
       return next(createError(404, 'use https'))
     }
