02841d6c5dc52d33091cc8cf390f80bab61f4831
James Abney <james.abney@gmail.com>
2019-12-07 13:48:02 -0800

    Create default-options.ts

---
+++ src/lib/default-options.ts
@@ -0,0 +1,18 @@
+/**
+ * Return a function that takes an options object and applies
+ * it to a default options object, e.g.,
+ *
+ *   ```
+ *   // Get a function configured with default options.
+ *   const myOptsFn = defaultOptions({ color: 'red', size: 15 })
+ *
+ *   // Override one or more of the default options.
+ *   const myOpts = myOptsFn({ size: 12 }) // { color: 'red', size: 12 }
+ *   ```
+ */
+export function defaultOptions<T>(defaults: T) {
+  const optionsFn = (options: T) => <T>Object.assign({}, defaults, options)
+  return optionsFn
+}
+
+export default defaultOptions
