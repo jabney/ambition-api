1419180af230a7ded51b0dbcadd0331b57a5961d
James Abney <james.abney@gmail.com>
2019-12-07 08:56:56 -0800

    Implement integerize function

--- src/lib/env-helpers.ts
+++ src/lib/env-helpers.ts
@@ -9,3 +9,21 @@ export function booleanize(value: string|undefined, compare = 'true') {
 
   return value.trim().toLowerCase() === compare.trim().toLowerCase()
 }
+
+/**
+ * Convert a string to a number.
+ */
+export function integerize(value: string|undefined, onUndefined: number|'throw' = 'throw') {
+  if (value == null) {
+    if (onUndefined === 'throw') {
+      throw new Error('<integerize> value is undefined')
+    }
+    return onUndefined
+  }
+
+  try {
+    return parseInt(value)
+  } catch (e) {
+    throw new Error(`<integerize> cannot parse "${value}" as integer`)
+  }
+}
