56c6340844db239c42e303bf8905562952095340
James Abney <james.abney@gmail.com>
2019-12-07 07:25:54 -0800

    Add error arg to constructor

--- src/lib/errors/http-error.ts
+++ src/lib/errors/http-error.ts
@@ -1,18 +1,17 @@
-export interface JSONError {
-  status: number
-  message: string
-}
+import { JSONError } from './json-error'
 
 export class HttpError extends Error {
   public status: number
+  public error: string|null
 
-  constructor(status=500, message='internal server error') {
+  constructor(status=500, message='internal server error', error: string|null = null) {
     super(message)
     this.status = status
+    this.error = error
   }
 
-  toJSON() {
-    const { status, message } = this
+  toJSON(): JSONError {
+    const { status, message, error } = this
     return { status, message }
   }
 }
