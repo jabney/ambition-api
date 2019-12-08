3d15aedb300ca15ad275443f2f7e7d613e3263c5
James Abney <james.abney@gmail.com>
2019-12-07 16:12:31 -0800

    Add 'name' member variable

--- src/lib/errors/http-error.ts
+++ src/lib/errors/http-error.ts
@@ -1,11 +1,13 @@
 import { JSONError } from './json-error'
 
 export class HttpError extends Error {
+  public name: string
   public status: number
   public wrapped: string|null
 
   constructor(status=500, message='internal server error', stack: string|null = null) {
     super(message)
+    this.name = 'HttpError'
     this.status = status
     this.wrapped = stack
   }
