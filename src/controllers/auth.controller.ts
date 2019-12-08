1fee9147f8b7c373762da2a9014230121590faac
James Abney <james.abney@gmail.com>
2019-12-07 19:10:48 -0800

    Implement signin method

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,8 @@
 import { RequestHandler } from 'express'
 import { Token } from '../models/token.model'
+import { User } from '../models/user.model'
 import * as tokens from '../lib/tokens'
+import { createError } from '../lib/errors'
 
 /**
  * Create a token and save it to the db.
@@ -30,8 +32,27 @@ export const signup: RequestHandler = (req, res, next) => {
 /**
  *
  */
-export const signin: RequestHandler = (req, res, next) => {
-  res.json({ data: 'signin' })
+export const signin: RequestHandler = async (req, res, next) => {
+  const { email, password } = req.body
+
+  try {
+    const user = await User.findOne({ email })
+
+    if (user == null) {
+      return next(createError(404, 'user not found'))
+    }
+
+    if (!await user.verifyPassword(password)) {
+      return next(createError(404, 'user not found'))
+    }
+
+    const token = await createToken(user._id.toHexString())
+
+    res.json({ token })
+
+  } catch (e) {
+    return next(createError(e))
+  }
 }
 
 /**
