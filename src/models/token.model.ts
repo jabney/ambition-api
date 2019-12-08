be163e8f45185eb39bcce0c213ed24fff307e31e
James Abney <james.abney@gmail.com>
2019-12-07 18:26:36 -0800

    Add schema options to token schema

--- src/models/token.model.ts
+++ src/models/token.model.ts
@@ -1,7 +1,6 @@
-import mongoose, { Document, Schema, Types, SchemaTypes } from 'mongoose'
+import mongoose, { Document, Schema, Types } from 'mongoose'
 import { IToken } from './token.interface'
-
-const { ObjectId } = SchemaTypes
+import schemaOptions from '../config/schema-options'
 
 export interface ITokenDocument extends IToken, Document {
   _id: Types.ObjectId
@@ -13,11 +12,11 @@ type TokenModel = mongoose.Model<ITokenDocument> & {/* tokenSchema.statics */}
 
 export const tokenSchema = new Schema({
   // The owner of the token.
-  userId: { $type: ObjectId, required: true },
+  userId: { $type: Schema.Types.ObjectId, required: true },
   // The time the token was issued.
   issuedAt: { $type: Number, required: true },
   // The time the token expires.
   expiresAt: { $type: Number, required: true },
-})
+}, schemaOptions())
 
 export const Token = <TokenModel>mongoose.model<ITokenDocument>('Token', tokenSchema)
