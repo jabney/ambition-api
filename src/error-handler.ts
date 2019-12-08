218a1015f96bf095ff76938223a53d53d09e5472
James Abney <james.abney@gmail.com>
2019-12-08 07:31:46 -0800

    Log all errors

--- src/error-handler.ts
+++ src/error-handler.ts
@@ -5,10 +5,11 @@ import { HttpError } from './lib/errors'
  * Main application error handler.
  */
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  console.log(err)
+
   if (err instanceof HttpError) {
     res.status(err.status).json(err)
   } else {
-    console.log(err)
     const status = 500
     const message = err.message || 'unknown error'
     res.status(status).json({ status, message })
