1963ffcb8e89a0fd6b05a99943db9305121c9a53
James Abney <james.abney@gmail.com>
2019-12-09 12:13:21 -0800

    Create grants.ts

---
+++ src/middleware/grants.ts
@@ -0,0 +1,25 @@
+import { RequestHandler } from 'express'
+import { createError } from '../lib/errors'
+
+/**
+ * Allow request if the user has granted the permission.
+ *
+ * This middleware requires the user to be deserialized with 'grants'.
+ */
+export function grants(permission: string): RequestHandler {
+  return (req, res, next) => {
+    const user = req.user
+
+    if (!user || !user.grants) {
+      return next(createError(500, 'user grants unavailable'))
+    }
+
+    if (user.grants.includes(permission)) {
+      next()
+    } else {
+      next(createError(403))
+    }
+  }
+}
+
+export default grants
