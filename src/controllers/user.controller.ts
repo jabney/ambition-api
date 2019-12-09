3fd298de0034e8abc4d89a60c33509457bf45bbe
James Abney <james.abney@gmail.com>
2019-12-09 10:53:17 -0800

    Implement user.controller.ts

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,38 @@
+import { RequestHandler } from 'express'
+import { createError } from '../lib/errors'
+
+/**
+ *
+ */
+export const fetch: RequestHandler = async (req, res, next) => {
+  res.json({ user: req.user })
+}
+
+/**
+ *
+ */
+export const update: RequestHandler = async (req, res, next) => {
+  const user = req.user
+  const fields = req.body
+
+  try {
+    await user.update(fields)
+    res.json({ data: 'user updated' })
+
+  } catch (e) {
+    next(createError(e))
+  }
+}
+
+/**
+ *
+ */
+export const remove: RequestHandler = async (req, res, next) => {
+  try {
+    await req.user.remove()
+    res.json({ data: 'user data deleted '})
+
+  } catch (e) {
+    next(createError(e))
+  }
+}
