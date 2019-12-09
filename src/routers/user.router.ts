a46da19dc5fb0038ab8a8d9ea2cf5f58e9ed1de1
James Abney <james.abney@gmail.com>
2019-12-09 10:53:34 -0800

    Define user routes

---
+++ src/routers/user.router.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express'
+import { fetch, update, remove } from '../controllers/user.controller'
+import { deserializeUser, tokenRequired } from '../middleware/authorize'
+import { updateValidator } from '../validators/user.validator'
+
+const router = Router()
+
+const deserializeProfile = deserializeUser('email first last grants roles')
+const deserializeMin = deserializeUser('_id')
+
+router.route('/')
+  .get(deserializeProfile, fetch)
+  .patch(deserializeMin, updateValidator, update)
+  .delete(tokenRequired, remove)
+
+export default router
