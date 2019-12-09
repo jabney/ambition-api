477184694158deee017faf2075bbf35df9648d26
James Abney <james.abney@gmail.com>
2019-12-08 18:41:42 -0800

    Add validator middleware

--- src/routers/auth.router.ts
+++ src/routers/auth.router.ts
@@ -2,19 +2,23 @@ import { Router } from 'express'
 import { apiKey } from '../middleware/api-key'
 import { signup, signin, signout, signoutAll } from '../controllers/auth.controller'
 import { tokenOptional, tokenRequired } from '../middleware/deserialize-user'
+import { signupValidator, signinValidator } from '../validators/auth.validator'
+
+const optionalToken = tokenOptional('_id')
+const requiredToken = tokenRequired('_id')
 
 const router = Router()
 
 router.route('/signup')
-  .post(apiKey, signup)
+  .post(apiKey, signupValidator, signup)
 
 router.route('/signin')
-  .post(apiKey, tokenOptional('_id'), signin)
+  .post(apiKey, optionalToken, signinValidator, signin)
 
 router.route('/signout')
-  .get(signout, tokenRequired('_id'))
+  .get(requiredToken, signout)
 
 router.route('/signout/all')
-  .get(signoutAll, tokenRequired('_id'))
+  .get(requiredToken, signoutAll)
 
 export default router
