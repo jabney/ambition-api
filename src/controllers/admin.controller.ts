a466668951fc442583c2231e50e47638e3608676
James Abney <james.abney@gmail.com>
2019-12-09 15:33:48 -0800

    Implement admin controller

---
+++ src/controllers/admin.controller.ts
@@ -0,0 +1,50 @@
+import { RequestHandler } from 'express'
+import { createError } from '../lib/errors'
+import { Whitelist } from '../models/whitelist-config.model'
+
+/**
+ *
+ */
+export const fetchWhitelist: RequestHandler = async (req, res, next) => {
+  try {
+    const doc = await Whitelist.findOne()
+    const whitelist = doc && doc.allowed || []
+
+    res.json({ whitelist })
+
+  } catch (e) {
+    next(createError(e))
+  }
+}
+
+/**
+ *
+ */
+export const addToWhtelist: RequestHandler = async (req, res, next) => {
+  const { email } = req.body
+
+  try {
+    await Whitelist.updateOne({}, { $addToSet: { allowed: email }}, { upsert: true })
+
+    res.json({ data: 'email added' })
+
+  } catch (e) {
+    return next(createError(e))
+  }
+}
+
+/**
+ *
+ */
+export const removeFromWhitelist: RequestHandler = async (req, res, next) => {
+  const { email } = req.body
+
+  try {
+    await Whitelist.updateOne({}, { $pull: { allowed: email }}, { upsert: true })
+
+    res.json({ data: 'email removed' })
+
+  } catch (e) {
+    return next(createError(e))
+  }
+}
