cf1b6654f5a257583bf54d712d1f2eb2a861c249
James Abney <james.abney@gmail.com>
2019-12-09 16:12:43 -0800

    Apply whitelist check on signin/signup

--- src/routers/auth.router.ts
+++ src/routers/auth.router.ts
@@ -2,15 +2,16 @@ import { Router } from 'express'
 import { apiKey } from '../middleware/api-key'
 import { signup, signin, signout, signoutAll } from '../controllers/auth.controller'
 import { tokenOptional, tokenRequired } from '../middleware/authorize'
+import { requireWhitelisted } from '../middleware/require-whitelisted'
 import { signupValidator, signinValidator } from '../validators/auth.validator'
 
 const router = Router()
 
 router.route('/signup')
-  .post(apiKey, signupValidator, signup)
+  .post(apiKey, requireWhitelisted, signupValidator, signup)
 
 router.route('/signin')
-  .post(apiKey, tokenOptional, signinValidator, signin)
+  .post(apiKey, requireWhitelisted, tokenOptional, signinValidator, signin)
 
 router.route('/signout')
   .get(tokenRequired, signout)
