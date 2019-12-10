611b8d8aa0c053695b1d56c2863c373eef0d1aab
James Abney <james.abney@gmail.com>
2019-12-10 07:30:37 -0800

    Restructure grant items as name, desc objects

--- src/config/grants.ts
+++ src/config/grants.ts
@@ -1,13 +1,29 @@
 export type GrantType = 'track-location'
 
-export const grants: ReadonlyArray<GrantType> = Object.freeze([
-  'track-location',
-])
-
-const set: ReadonlySet<GrantType> = new Set(grants)
-
-export function isValidGrant(grant: GrantType) {
-  return set.has(grant)
+export interface IGrantItem {
+  type: GrantType
+  description: string
 }
 
-export default grants
+const grantItems: IGrantItem[] = [
+  {
+    type: 'track-location',
+    description: 'allow location data to be gathered and stored',
+  },
+]
+
+// Create a set of grant types (type only, no description).
+const grantTypes: ReadonlySet<GrantType> = new Set(grantItems.map(({ type }) => type))
+
+/**
+ * Test the validity of a grant type.
+ */
+export function isValidGrant(type: GrantType) {
+  return grantTypes.has(type)
+}
+
+// Export a copy of the grant items.
+export const grantList: IGrantItem[] = grantItems.map(item => ({ ...item }))
+
+// Export a list of grant types.
+export const grants: GrantType[] = grantList.map(({ type }) => type)
