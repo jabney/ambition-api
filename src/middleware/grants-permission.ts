c54f056a60d0c9bac846a468cf67c4d3f1713698
James Abney <james.abney@gmail.com>
2019-12-09 16:43:09 -0800

    Implement grantsPermission middleware

---
+++ src/middleware/grants-permission.ts
@@ -0,0 +1,30 @@
+import { RequestHandler } from 'express'
+import { createError } from '../lib/errors'
+import { isValidGrant } from '../config/grants'
+
+/**
+ * Allow request if the user has the given grant.
+ *
+ * This middleware requires the user to be deserialized with 'grants'.
+ */
+export function grantsPermission(grant: string): RequestHandler {
+  if (!isValidGrant(grant)) {
+    throw new Error(`<grantsPermission> grant "${grant}" is not valid`)
+  }
+
+  return (req, res, next) => {
+    const user = req.user
+
+    if (!user || !user.grants) {
+      return next(createError(500, 'user grants unavailable'))
+    }
+
+    if (user.grants.includes(grant)) {
+      next()
+    } else {
+      next(createError(403))
+    }
+  }
+}
+
+export default grantsPermission
