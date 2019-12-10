260f0ab82e5679f007126ce536f7d1ef399bddd5
James Abney <james.abney@gmail.com>
2019-12-09 16:12:03 -0800

    Implement requireWhitelisted middleware

---
+++ src/middleware/require-whitelisted.ts
@@ -0,0 +1,20 @@
+import { RequestHandler } from 'express'
+import { createError } from '../lib/errors'
+import { Whitelist } from '../models/whitelist-config.model'
+
+/**
+ * Require that the email address in the request body be whitelisted.
+ */
+export const requireWhitelisted: RequestHandler = async (req, res, next) => {
+  const { email } = req.body
+
+  try {
+    if (!await Whitelist.isWhitelisted(email)) {
+      next(createError(403))
+    } else {
+      next()
+    }
+  } catch (e) {
+    next(createError(e))
+  }
+}
