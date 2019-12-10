6f2965b05aec5126cf5936cf85453e438133e69d
James Abney <james.abney@gmail.com>
2019-12-10 11:02:18 -0800

    Add env variable

--- src/environment.ts
+++ src/environment.ts
@@ -8,6 +8,7 @@ import {
 const PRODUCTION = booleanize(process.env.NODE_ENV, 'production')
 const DEVELOPMENT = !PRODUCTION
 
+const MONGODB_URI = process.env.MONGODB_URI
 const ALLOW_UNSECURE = booleanize(process.env.ALLOW_UNSECURE)
 const TRUST_PROXY = integerize(process.env.TRUST_PROXY, 0)
 const API_KEY = process.env.API_KEY
@@ -22,6 +23,7 @@ const PASSWORD_COST_FACTOR = integerize(process.env.PASSWORD_COST_FACTOR, 10)
 export default Object.freeze({
   DEVELOPMENT,
   PRODUCTION,
+  MONGODB_URI,
   /**
    * Don't reject non-https requests.
    */
