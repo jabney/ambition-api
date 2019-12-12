eb2fceb5dc0a84414fecfe313da685ac7502d289
James Abney <james.abney@gmail.com>
2019-12-12 09:30:26 -0800

    Add ttl index to token schema

--- src/models/token.model.ts
+++ src/models/token.model.ts
@@ -2,6 +2,11 @@ import mongoose, { Document, Schema, Types } from 'mongoose'
 import schemaOptions from '../config/schema-options'
 import * as tokens from '../lib/tokens'
 import { IToken } from './token.interface'
+import { timeSec } from '../lib/time'
+import env from '../environment'
+
+// The expire time for the ttl index; append 's' for seconds.
+const expires = timeSec({ days: env.JWT_EXPIRES_IN_DAYS }) + 's'
 
 export interface ITokenDocument extends IToken, Document {
   _id: Types.ObjectId
@@ -23,9 +28,12 @@ export const tokenSchema = new Schema({
   issuedAt: { $type: Number, required: true },
   // The time the token expires.
   expiresAt: { $type: Number, required: true },
+  // Used for ttl.
+  createdAt: { $type: Date, default: Date.now },
 }, schemaOptions())
 
 tokenSchema.index({ userId: 1 })
+tokenSchema.index({ createdAt: 1 }, { expires })
 
 /**
  * Create and return a json web token.
