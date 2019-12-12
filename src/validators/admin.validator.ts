91f15217ed9cc9177980d4cbecea8a23c8a5f877
James Abney <james.abney@gmail.com>
2019-12-12 11:16:59 -0800

    Create revoke validators

--- src/validators/admin.validator.ts
+++ src/validators/admin.validator.ts
@@ -1,14 +1,27 @@
 import validationErrors from '../middleware/validation-errors'
 import defs from './definitions'
 
-export const addValidator = [
+export const addToWhitelistValidator = [
   defs.body.email(),
 
   validationErrors,
 ]
 
-export const removeValidator = [
+export const removeFromWhitelistValidator = [
   defs.body.email(),
+
+  validationErrors,
+]
+
+export const revokeTokensValidator = [
+  defs.body.userId().optional(),
+  defs.body.email().optional(),
+
+  validationErrors,
+]
+
+export const revokeAllTokensValidator = [
+  defs.body.confirm().optional(),
 
   validationErrors,
 ]
