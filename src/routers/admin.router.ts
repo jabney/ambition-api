502467ca4a8e3d93a49af2175e3c024a4d5e9089
James Abney <james.abney@gmail.com>
2019-12-12 11:17:13 -0800

    Mount validation middleware

--- src/routers/admin.router.ts
+++ src/routers/admin.router.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express'
 import { deserializeUser } from '../middleware/authorize'
 import hasRole from '../middleware/has-role'
-import { addValidator, removeValidator } from '../validators/admin.validator'
+
+import {
+  addToWhitelistValidator,
+  removeFromWhitelistValidator,
+  revokeTokensValidator,
+  revokeAllTokensValidator,
+} from '../validators/admin.validator'
 
 import {
   addToWhtelist,
@@ -20,13 +26,13 @@ router.use(deserializeUser('roles'))
 
 router.route('/whitelist')
   .get(hasAdmin, fetchWhitelist)
-  .post(hasAdmin, addValidator, addToWhtelist)
-  .delete(hasAdmin, removeValidator, removeFromWhitelist)
+  .post(hasAdmin, addToWhitelistValidator, addToWhtelist)
+  .delete(hasAdmin, removeFromWhitelistValidator, removeFromWhitelist)
 
 router.route('/tokens/revoke')
-  .delete(hasAdmin, revokeTokens)
+  .delete(hasAdmin, revokeTokensValidator, revokeTokens)
 
 router.route('/tokens/revoke/all')
-  .delete(hasSuper, revokeAllTokens)
+  .delete(hasSuper, revokeAllTokensValidator, revokeAllTokens)
 
 export default router
