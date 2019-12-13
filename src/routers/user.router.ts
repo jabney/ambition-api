423377f21db88498f6f662cca4af8e2be173fb4f
James Abney <james.abney@gmail.com>
2019-12-12 17:01:22 -0800

    Mount user routes

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -3,6 +3,7 @@ import { deserializeUser } from '../middleware/authorize'
 
 import {
   updateUserValidator,
+  updateSettingsValidator,
   addGrantValidator,
   removeGrantValidator,
 } from '../validators/user.validator'
@@ -11,6 +12,10 @@ import {
   fetchUser,
   updateUser,
   deleteUser,
+
+  fetchSettings,
+  updateSettings,
+  deleteSettings,
 
   fetchGrants,
   addGrant,
@@ -21,16 +26,22 @@ const router = Router()
 
 const deserializeProfile = deserializeUser('email first last grants roles')
 const deserializeMin = deserializeUser('_id')
-const deserializeStd = deserializeUser('grants')
+const deserializeGrants = deserializeUser('grants')
+const deserializeSettings = deserializeUser('settings')
 
 router.route('/')
   .get(deserializeProfile, fetchUser)
   .patch(deserializeMin, updateUserValidator, updateUser)
   .delete(deserializeMin, deleteUser)
 
+router.route('/settings')
+  .get(deserializeSettings, fetchSettings)
+  .post(deserializeSettings, updateSettingsValidator, updateSettings)
+  .delete(deserializeSettings, deleteSettings)
+
 router.route('/grants')
-  .get(deserializeStd, fetchGrants)
-  .post(deserializeStd, addGrantValidator, addGrant)
-  .delete(deserializeStd, removeGrantValidator, removeGrant)
+  .get(deserializeGrants, fetchGrants)
+  .post(deserializeGrants, addGrantValidator, addGrant)
+  .delete(deserializeGrants, removeGrantValidator, removeGrant)
 
 export default router
