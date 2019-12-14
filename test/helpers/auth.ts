0bd85f500bba44836b2de7c3510f743e69a87136
James Abney <james.abney@gmail.com>
2019-12-14 10:09:00 -0800

    Update signinUser to take an optional token

--- test/helpers/auth.ts
+++ test/helpers/auth.ts
@@ -3,18 +3,32 @@ import app from '../../src/app'
 import { IProfileInfo } from '../../src/models/profile-info.interface'
 import { authHeader } from './auth-header'
 
+/**
+ *
+ */
 export function signupUser(profile: IProfileInfo) {
   return request(app)
     .post('/auth/signup')
     .send(profile)
 }
 
-export function signinUser(credentials: IProfileInfo) {
-  return request(app)
+/**
+ *
+ */
+export function signinUser(credentials: IProfileInfo, token?: string) {
+  const req = request(app)
     .post('/auth/signin')
-    .send(credentials)
+
+  if (token) {
+    req.set(...authHeader(token))
+  }
+
+  return req.send(credentials)
 }
 
+/**
+ *
+ */
 export function signoutUser(token: string) {
   return request(app)
     .get('/auth/signout')
