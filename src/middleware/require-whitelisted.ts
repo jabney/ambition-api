35a3b59e377d565c11fa4f88ca80a9b79c593eee
James Abney <james.abney@gmail.com>
2019-12-14 09:11:47 -0800

    Bypass check on environment variable

--- src/middleware/require-whitelisted.ts
+++ src/middleware/require-whitelisted.ts
@@ -1,12 +1,17 @@
 import { RequestHandler } from 'express'
 import { createError } from '../lib/errors'
 import { Whitelist } from '../models/whitelist.model'
+import env from '../environment'
 
 /**
  * Require that the email address in the request body be whitelisted.
  */
 export const requireWhitelisted: RequestHandler = async (req, res, next) => {
   const { email } = req.body
+
+  if (!env.REQUIRE_WHITELISTED) {
+    return next()
+  }
 
   try {
     if (!await Whitelist.isWhitelisted(email)) {
