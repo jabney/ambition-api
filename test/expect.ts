9f0cba543588f7d927bd2238446f390ebc473570
James Abney <james.abney@gmail.com>
2019-12-14 10:06:53 -0800

    Implement expectTokenCount function

--- test/expect.ts
+++ test/expect.ts
@@ -2,14 +2,26 @@ import assert from 'assert'
 import { Response } from 'supertest'
 import { decode as decodeToken } from '../src/lib/tokens'
 import env from '../src/environment'
+import { countTokens } from './helpers/db-utils'
 
-export const expectToken = () => {
-  return (res: Response) => {
-    const { token } = res.body
-    assert.strictEqual(typeof token, 'string', 'token is a string')
+/**
+ * Expect a valid token.
+ */
+export const validToken = () => (res: Response) => {
+  const { token } = res.body
+  assert.strictEqual(typeof token, 'string', 'token is a string')
 
-    const { iss: issuer, aud: audience } = decodeToken(token)
-    assert.strictEqual(issuer, env.JWT_ISSUER, 'issuer is present')
-    assert.strictEqual(audience, env.JWT_AUDIENCE, 'audience is present')
+  const { iss: issuer, aud: audience } = decodeToken(token)
+  assert.strictEqual(issuer, env.JWT_ISSUER, 'issuer is present')
+  assert.strictEqual(audience, env.JWT_AUDIENCE, 'audience is present')
+}
+
+/**
+ * Expect a number of tokens to be in the Tokens collection.
+ */
+export const expectTokenCount = (expected: number, query: any = {}) => {
+  return async (res: Response) => {
+    const actual = await countTokens(query)
+    assert.strictEqual(actual, expected, `expected ${expected} tokens but found ${actual}`)
   }
 }
