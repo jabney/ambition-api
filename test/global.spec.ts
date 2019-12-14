f4bb49fe66f784534e31229226396b60ca5b2168
James Abney <james.abney@gmail.com>
2019-12-14 09:03:27 -0800

    Remove global whitelist management

--- test/global.spec.ts
+++ test/global.spec.ts
@@ -1,12 +1,9 @@
 import { connection } from 'mongoose'
-import { deleteTestData, addToWhitelist } from './helpers/db-utils'
-import users from './helpers/user-profile'
+import { deleteTestData } from './helpers/db-utils'
 
-const emails = Object.values(users).map(user => user.email)
 
 beforeEach(async () => {
   await deleteTestData()
-  await addToWhitelist(emails)
 })
 
 after(async () => {
