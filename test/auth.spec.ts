e62a9b28e2d328e84e182d5bfe55b1f5e787bfe9
James Abney <james.abney@gmail.com>
2019-12-14 09:03:45 -0800

    Manually add to whitelist before signup

--- test/auth.spec.ts
+++ test/auth.spec.ts
@@ -1,16 +1,19 @@
 import { signupUser, signinUser } from './helpers/auth'
-import { userProfile, userCredentials } from './helpers/user-profile'
+import { userEmails, userProfile, userCredentials } from './helpers/user-profile'
 import { expectToken } from './expect'
+import { addToWhitelist } from './helpers/db-utils'
 
 describe('Auth Routes', () => {
 
   it('signs up a user', async () => {
+    await addToWhitelist(userEmails)
     await signupUser(userProfile())
       .expect(200)
       .expect(expectToken())
   })
 
   it('signs in a user', async () => {
+    await addToWhitelist(userEmails)
     await signupUser(userProfile())
       .expect(200)
 
