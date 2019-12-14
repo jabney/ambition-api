991a461b0cf0fe46d24d3ee06195f5d0af6c3111
James Abney <james.abney@gmail.com>
2019-12-14 10:26:36 -0800

    Update assert messages

--- test/expect.ts
+++ test/expect.ts
@@ -12,8 +12,8 @@ export const validToken = () => (res: Response) => {
   assert.strictEqual(typeof token, 'string', 'token is a string')
 
   const { iss: issuer, aud: audience } = decodeToken(token)
-  assert.strictEqual(issuer, env.JWT_ISSUER, 'issuer is present')
-  assert.strictEqual(audience, env.JWT_AUDIENCE, 'audience is present')
+  assert.strictEqual(issuer, env.JWT_ISSUER, `expected ${env.JWT_ISSUER} tokens but found ${issuer}`)
+  assert.strictEqual(audience, env.JWT_AUDIENCE, `expected ${env.JWT_AUDIENCE} tokens but found ${audience}`)
 }
 
 /**
