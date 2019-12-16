fc8f531942d10945cf2ec0dc32185dee1979c25b
James Abney <james.abney@gmail.com>
2019-12-16 08:18:54 -0800

    Implement apiKeyHeader function

--- test/helpers/utils.ts
+++ test/helpers/utils.ts
@@ -1,4 +1,9 @@
+import env from '../../src/environment'
 
 export const authHeader = (token: string): [string, string] => {
   return ['Authorization', `Bearer ${token}`]
+}
+
+export const apiKeyHeader = (): [string, string] => {
+  return ['Authorization', `Bearer ${env.API_KEY}`]
 }
